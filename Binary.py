748d202c6ae2ca5734a679c17c5b2a8ba3f57fd8
SherSingh Gola <89210391+shersinghgola0001@users.noreply.github.com>
2023-10-06 20:09:14 +0530

    Create Binary.py

---
+++ Binary.py
@@ -0,0 +1,11 @@
+# Python program to introduce Binary Tree
+class Node:
+	def __init__(self, key):
+		self.left = None
+		self.right = None
+		self.val = key
+if __name__ == '__main__':
+	root = Node(1)
+	root.left = Node(2)
+	root.right = Node(3)
+	root.left.left = Node(4)
